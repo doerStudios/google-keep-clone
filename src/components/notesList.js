1786a2c8fc19cb5eb3fcc2cbce2a3bfb428ae66c
doerStudios <contact.doerstudios@gmail.com>
2021-08-09 15:29:55 +0800

    search filter

--- src/components/notesList.js
+++ src/components/notesList.js
@@ -1,27 +1,54 @@
 import React, { useContext } from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import { Col, Row, Form, Container } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import NotesContext from "../context/notes-context";
 import NoteItem from "./noteItem";
+import { includes } from "lodash";
 
 function NotesList(props) {
   const context = useContext(NotesContext);
-  let notes = context.notes.map((x, i) => {
-    return (
-      <NoteItem
-        key={x.id}
-        id={x.id}
-        title={x.title}
-        note={x.note}
-        index={i}
-        editNote={(note) => {
-          props.editNote(note);
-        }}
-      />
-    );
-  });
+  const { register, handleSubmit, watch } = useForm({});
+
+  let notes = context.notes
+    .filter((x) => {
+      return (
+        includes(x.title, watch("filterNote")) ||
+        includes(x.note, watch("filterNote"))
+      );
+    })
+    .map((x, i) => {
+      return (
+        <NoteItem
+          key={x.id}
+          id={x.id}
+          title={x.title}
+          note={x.note}
+          index={i}
+          editNote={(note) => {
+            props.editNote(note);
+          }}
+        />
+      );
+    });
 
   return (
     <>
+      <Container>
+        <Row>
+          <Col md={8} className="mx-auto">
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  size="lg"
+                  placeholder="search.."
+                  {...register("filterNote")}
+                />
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
       <Row className="notes__card">{notes}</Row>
     </>
   );
